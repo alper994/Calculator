7e5f18c1f2a22ea789f872ee82af096b74d4f41a
alper994 <alperdelibas1994@gmail.com>
2026-07-10 15:03:46 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Alpi's Mathe")
+
+first_digit = st.number_input("Erste Zahl")
+operator = st.selectbox("Operator", ["+", "-", "*", "/"])
+second_digit = st.number_input("Zweite Zahl")
+
+if st.button("Berechnen"):
+
+    if operator == "+":
+        result = first_digit + second_digit
+
+    elif operator == "-":
+        result = first_digit - second_digit
+
+    elif operator == "*":
+        result = first_digit * second_digit
+
+    elif operator == "/":
+        if second_digit == 0:
+            st.error("Division durch 0 geht nicht")
+            st.stop()
+        result = first_digit / second_digit
+
+    st.write(f"{first_digit:g} {operator} {second_digit:g} = {result:g}")
